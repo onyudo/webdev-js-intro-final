5aa5ab17a0cf78a614ebf3ce5cea0b6991ddbdc8
“Martin <martin@forceandform.com>
2024-12-27 16:24:13 -0500

    Created submiisionBtn, restartBtn and guessInput variables. Created base render function. Added eventListeners for submissionBtn and restartBtn.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
 "use strict";
+
+const submissionBtn = document.getElementById("submit-btn");
+const restartBtn = document.getElementById("restart-btn");
+const guessInput = document.getElementById("guess-input");
+
+function restart() {
+    count = 0; // reset the count to zero
+    
+}
+
+function render() {
+    restart();
+}
+
+
+submissionBtn.addEventListener("click", function () {
+    render();
+});
+
+restartBtn.addEventListener("click", function () {
+    restart();
+});
