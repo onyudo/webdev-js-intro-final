38be1249ecde40d9f099db847a3584a3eae8634b
“Martin <martin@forceandform.com>
2024-12-30 13:42:57 -0500

    Created a large Else If statement in the checkGuess function that compares player input to the randomaly generted number, esnures that only 3 guesses are given and outputs expected messages to the html document. Updated README to indicate that most of the expected outputs and functionality has been accounted for. Need to figure out elemts of game reset to complete.

--- js/app.js
+++ js/app.js
@@ -14,10 +14,12 @@ const guessHistory = document.getElementById("guess-history");
 
 // let the program choose a random number, an empty variable?
 let randomNumber;
+
 // let the program keep track of attempts, starting at zero
 let attempts = 0;
-// let the program keep track of the number guessed, probably using an array
-// will need to push guesses into this array and output them, probably in the checkGuess function
+
+// let the program keep track of the numbers guessed, probably using an array
+// will need to push guesses into this array and output them, probably in the checkGuess function. YES, YOU WILL
 let guessHistoryList = [];
 
 // function start the game
@@ -36,9 +38,10 @@ function startGame() {
     // keep track of attempts? starts at zero at game start
     // do I really need this since I declare the same value above?
     // might be necessary since I am saying that the restart function calls the startGame function
-    attempts = 0; // reset attempts?
+    attempts = 0; // reset attempts? YES, LEAVE THIS
 
-    // Do I need to clear off other display messages from the previous game?
+    // Do I need to clear off other display messages and variables from a previous game?
+    // YES, I DO
 
     // gotta do something about the enabled/disbale state of the buttons?
     // Button initial states at start of game
@@ -58,7 +61,7 @@ function checkGuess() {
     // get the value from the guessInput field, convert that value (which is a string) into a number, and then assign the result to the variable playerGuess
     const playerGuess = parseInt(guessInput.value);
 
-    // Validate that the input is a number, use isNaN
+    // Validate that the input is a number and a number in the correct range, use isNaN
     if (isNaN(playerGuess) || playerGuess < 1 || playerGuess > 10) {
         guessMessage.innerText = "Please enter a number between 1 and 10.";
         return;
@@ -74,10 +77,25 @@ function checkGuess() {
     guessHistory.innerText = guessHistoryList.join(", ");
 
     // Check if the input guess is correct
-        // is guess equal to random number?
+    // is guess equal to random number?
+    if (playerGuess === randomNumber) {
         // if yes: "you win" or whatever and end the game
+        guessMessage.innerText = "Congratulations! You guessed the correct number!";
+        currentGuess.innerText = playerGuess;
+        computerGuess.innerText = randomNumber;
+        endGame();
         // else if not correct and number of guesses is greater than or equal to 3 "you loose, the number was x"
-        // else your input was too low or too high, guess input compared to randomly chosen number
+        } else if (attempts >= 3) {
+            // below: the message would not display with "" but worked when I used ``
+            guessMessage.innerText =`Sorry, you've run out of attempts. The correct number was ${randomNumber}`;
+            currentGuess.innerText = playerGuess;
+            computerGuess.innerText = randomNumber;
+            endGame();
+        } else {
+        // else your input was too low or too high, guess input compared to randomly chosen number, use ternary operator
+        guessMessage.innerText = playerGuess < randomNumber ? "Too low!" : "Too high!";
+        currentGuess.innerText = playerGuess;
+        }
 }
 
 // function for ending the game. Disables the submit button and enables the restart button
